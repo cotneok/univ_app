362c4cff970c7f4d5a221bab82a35f8c2d4804a3
Tsotne <cotne.ok@gmail.com>
2019-09-08 15:04:40 +0400

    Add password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_172718) do
+ActiveRecord::Schema.define(version: 2019_09_08_105617) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_08_29_172718) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
